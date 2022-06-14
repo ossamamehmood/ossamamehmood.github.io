8dd4f51c0b55948d893457e0e5e2006361672d71
ossamamehmood <ossamamehmood110@gmail.com>
2022-06-14 19:55:27 +0500

    ossamamehmood

---
+++ about.php
@@ -0,0 +1,39 @@
+</section>
+    <!-- section menu end -->
+ <section id="section-overview" class="section-padding">
+		<div class="container">
+			<div class="row">
+						<div class="service-wrapper">
+							<div class="service-inner wow fadeInRight" data-wow-delay=".2s">
+								<i class="fa fa-users"></i>
+								<div class="service-box">
+									<h3>User Story</h3>
+									<p>Sole Proprietorship & Founder @Minimsam Ξ @Nftssam. Started a multiple ownership Non-Fungible token collection on behalf of creative/design skills set across the opensea. I have been trying out different Web 3.0 to get early access and achieve something in metaverse.</p>
+								</div>
+							</div> <!-- service -inner  -->
+							<div class="service-inner wow fadeInRight" data-wow-delay=".5s">
+								<i class="fa fa-compass"></i>
+								<div class="service-box">
+									<h3>Digital creator</h3>
+									<p>As a Digital Creator working with me, you’ll have round the clock access to new ideas, quick changes and high-quality content. I believe in getting to know all my customers personally and really understanding a brand to ensure the maximum effect</p>
+								</div>
+							</div>  <!-- service -inner  -->
+							<div class="service-inner wow fadeInRight" data-wow-delay=".8s">
+								<i class="fa fa-pencil"></i>
+								<div class="service-box">
+									<h3>Graphic Design</h3>
+									<p>Started my career as a Graphic Designer. I have excellent four plus years of hands-on experience gained throughout freelance across different platforms. Got a Fabulous amount of experience within year and reach some instance throughout freelance</p>
+								</div>
+							</div> <!-- service -inner  -->
+							<div class="service-inner wow fadeInRight" data-wow-delay="1.1s">
+								<i class="fa fa-rocket"></i>
+								<div class="service-box">
+									<h3>User Interface & Experience Design</h3>
+									<p>I was so cursed to learn and being a part currently learning User Interface & Experience Design (UI/UX). I have decided to enrolled in a twice a year course from Google (Google UX Design). In my wish list still User Interface/Experience is a flow to grow</p>
+								</div>
+							</div>  <!-- service -inner  -->
+						</div>
+					</div><!-- row end -->
+		</div>
+	</section>
+	<!-- section overview end -->
