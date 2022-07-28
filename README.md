3e2f12f17b547bd478a8621d0d5f54b1b274559c
ossamamehmood <ossamamehmood110@gmail.com>
2022-07-28 15:46:13 +0500

    ossamamehmood

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
-<!--- ossamamehmood.github.io | Preview --->
-  
+<div align="center">
+
+# Ossama Mehmood 샘 `(ossamamehmood.me)`
+
 <p align="left">
-  <img alt="" style="{max-height: 0px}" src="./images/Prototype.png">
+  <img alt="" style="{max-height: 20px}" src="./Prototype/Prototype (ossamamehmood).png">
 </p>
+  
+##                                                 `Greetings`..!
+######                                              I'm `Ossama Mehmood`
+######                                              `DIGITAL CREATOR`
+
+# `OSSAMAMEHMOOD`
+### `Home`
+### `About me`
+### `Skills`
+### `Resume`
+### `Testimonials`
+### `Portfolio`
+### `Contact me`
+
+<p align="left">
+  <img alt="" style="{max-height: 20px}" src="./Prototype/Website Preview/Website Preview.png">
+</p>
+
+## © `2022` © Ossama Mehmood. `Github` by `ossamamehmood`
+
+##### This is `Personal Project` is `Successfully Proceeded` 
+
+##### `Right` Are Reserved `©`2022 `ossamamehmood®`
+  
+</div>
---
+++ resume.php
@@ -0,0 +1,210 @@
+	<!-- Resume start -->
+	<section id="section-resume" class="section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center wow fadeInUp">
+					<h2 class="heading-title">Resume</h2>
+					<p>Freelancer / Software Engineer / Digital Creator / Graphic Designer / User Interface & Experience Designer</p>
+ 
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="resume-details" class="section-padding">
+	
+	
+<div class="container">
+<div class="row">
+    <div class="col-md-12 wow fadeInLeft">
+					
+					<div class="resume-inner">
+					<h2 class="heading-title">My Education</h2>
+						<p>
+						Present Statistic : Fifth Semester - Bachelour in Software Engineering (BSSE) - Comsats University Islamabad 
+						</p>
+					</div>
+				</div>
+</div>
+	<div class="row">
+
+        <div class="timeline-centered">
+
+        <article class="timeline-entry">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon bg-success">
+                    <i class="entypo-feather"></i>
+                </div>
+
+                <div class="timeline-label">
+                    <h3 class="resume-title">Bachelor in Software Engineering</h3>  
+                    <p>Undergraduate, Bachelor in Software Engineering (BSSE)</p>
+					<p>Comsats University Islamabad</p>
+					<p>2020-2024</p>
+                </div>
+            </div>
+
+        </article>
+
+
+        <article class="timeline-entry">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon bg-secondary">
+                    <i class="entypo-suitcase"></i>
+                </div>
+
+                <div class="timeline-label">
+                   <h3 class="resume-title">Intermediate in Computer Science</h3>
+                    <p>Higher Secondary School Certificate, Intermediate in Computer Science (ICS)</p>
+					<p>Punjab College of Commerce</p>
+					<p>2017-2019</p>
+                </div>
+            </div>
+
+        </article>
+
+
+        <article class="timeline-entry">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon bg-info">
+                    <i class="entypo-location"></i>
+                </div>
+
+                <div class="timeline-label">
+                   <h3 class="resume-title">Secondary School Certificate or Matriculation examination</h3>
+                    <p>Secondary School Certificate, Computer Science (CS)</p>
+					<p>Islamabad Model College For Boys</p>
+					<p>2015-2017</p>
+                </div>
+            </div>
+
+        </article>
+
+
+        <article class="timeline-entry begin">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon" style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg);">
+                    <i class="entypo-flight"></i> +
+                </div>
+
+            </div>
+
+        </article>
+
+    </div>
+
+    
+	</div>
+</div>
+	
+<div class="container">
+<div class="row">
+    <div class="col-md-12 wow fadeInLeft">
+					
+					<div class="resume-inner">
+					<h2 class="heading-title">Work Experience</h2>
+						<p>Present Statistic : Currently working as a freelancer/remote worker on freelance platforms (Upwork/Fiverr) aparts that building non-fungaible token collects</Non-Fungaible></p>
+					</div>
+				</div>
+</div>
+	<div class="row">
+
+        <div class="timeline-centered">
+
+        <article class="timeline-entry">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon bg-success">
+                    <i class="entypo-feather"></i>
+                </div>
+
+                <div class="timeline-label">
+                    <h3 class="resume-title">Sr. Web designer</h3>  
+                    <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                </div>
+            </div>
+
+        </article>
+
+
+        <article class="timeline-entry">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon bg-secondary">
+                    <i class="entypo-suitcase"></i>
+                </div>
+
+                <div class="timeline-label">
+                   <h3 class="resume-title">Web designer</h3>
+                     <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                </div>
+            </div>
+
+        </article>
+
+
+        <article class="timeline-entry">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon bg-info">
+                    <i class="entypo-location"></i>
+                </div>
+
+                <div class="timeline-label">
+                    <h3 class="resume-title">Graphics designer</h3>
+                    <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                </div>
+            </div>
+
+        </article>
+
+
+        <article class="timeline-entry">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon bg-warning">
+                    <i class="entypo-camera"></i>
+                </div>
+
+                <div class="timeline-label">
+                    <h3 class="resume-title">developer</h3>
+
+                    <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                </div>
+            </div>
+
+        </article>
+
+
+        <article class="timeline-entry begin">
+
+            <div class="timeline-entry-inner">
+
+                <div class="timeline-icon" style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg);">
+                    <i class="entypo-flight"></i> +
+                </div>
+
+            </div>
+
+        </article>
+
+    </div>
+
+    <a href="https://drive.google.com/file/d/19Rp11dDbqcDEPFQHjQ86Z2EE3e-cN9Ig/view?usp=sharing" class="btn btn-default featured">Download Resume</a>
+	</div>
+</div>
+	 
+	</section>
+	<!-- Resume end -->
