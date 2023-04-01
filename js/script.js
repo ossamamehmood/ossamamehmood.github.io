b205e17724488f140f122278912d03eb795ba900
Ossama Mehmood 샘 <ossamamehmood110@gmail.com>
2023-04-01 22:03:27 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,3 @@ document.getElementById("toggle").addEventListener("click", function()
 {
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
 });
-
-// Set the initial zoom level to 60%
-document.body.style.zoom = 0.6
