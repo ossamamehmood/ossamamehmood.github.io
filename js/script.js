5f7031ade68cdeb215ae8911c8e59b32b9fe9521
Ossama Mehmood 샘 <ossamamehmood110@gmail.com>
2023-04-01 21:35:38 +0500

    ossamamehmood

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ document.getElementById("toggle").addEventListener("click", function()
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
 });
 
-// Set the initial zoom level to 80%
-document.body.style.zoom = "80%";
+// Set the initial zoom level to 60%
+document.body.style.zoom = "60%";
+
+$("input, textarea").focusout(function(){
+    $('meta[name=viewport]').remove();
+    $('head').append('<meta name="viewport" content="width=device-width, initial-scale=0.6, maximum-scale=1.0, user-scalable=0">');
+
+    $('meta[name=viewport]').remove();
+    $('head').append('<meta name="viewport" content="width=device-width, initial-scale=yes">' );
+});
