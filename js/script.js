b21438d8d61cfe6a3dfbe2b4bc97e270ccd40da0
Ossama Mehmood 샘 <ossamamehmood110@gmail.com>
2023-04-01 21:44:35 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,12 +4,4 @@ document.getElementById("toggle").addEventListener("click", function()
 });
 
 // Set the initial zoom level to 60%
-document.body.style.zoom = "60%";
-
-$("input, textarea").focusout(function(){
-    $('meta[name=viewport]').remove();
-    $('head').append('<meta name="viewport" content="width=device-width, initial-scale=0.6, maximum-scale=1.0, user-scalable=0">');
-
-    $('meta[name=viewport]').remove();
-    $('head').append('<meta name="viewport" content="width=device-width, initial-scale=yes">' );
-});
+document.body.style.zoom = 0.6
